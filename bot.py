965bb7b0af9692f2707f17e7e7f3f29b3e030ee0
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-08-13 13:18:57 +0530

    20 more topics added

--- bot.py
+++ bot.py
@@ -7,22 +7,21 @@ from base64 import decodebytes
 
 TOKEN = os.environ.get("TOKEN")
 PORT = os.environ.get("PORT",5000)
+
+topics = ['gk', 'books', 'film', 'music', 'theaters', 'television', 'video_games', 'board_games', 'science_and_nature', 'computers', 'maths', 'mythology', 'sports', 'geography', 'history', 'politics', 'art', 'celebrities', 'animals', 'vehicles', 'comics', 'science_gadgets', 'japanese_anims', 'cartoon_animations']
+
 def start(update,context):
-    message = "Hi {} !! , Enjoy with the bot.".format(update.message.from_user.first_name)
+    message = "Hi {} !! ,This bot is developed by @TheShubhendra\nEnjoy with the bot.".format(update.message.from_user.first_name)
     context.bot.send_message(update.message.chat_id,message)
   
 def fetch(update,context):
   
   
   text= update.message.text
-  if "science" in text:
-    cat  ="17"
-  if "computer" in text:
-    cat = "18"
-  elif "maths" in text:
-    cat = "19"
-  elif "history" in text:
-    cat = "23"
+  for x in topics:
+    if x in text:
+      cat = topics.index(x)+9
+
   diff=["easy","medium","hard"]
   url ="https://opentdb.com/api.php?amount=1&category={}&difficulty={}&type=multiple&encode=base64".format(cat,diff[random.randint(0,2)])
   print(url)
@@ -45,7 +44,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 updater = Updater(TOKEN,use_context=True)
 dispatcher = updater.dispatcher
 
-handler = CommandHandler(["science","computer","history","maths"],fetch)
+handler = CommandHandler(topics,fetch)
 start_handler = CommandHandler('start',start)
 dispatcher.add_handler(handler)
 dispatcher.add_handler(start_handler)
